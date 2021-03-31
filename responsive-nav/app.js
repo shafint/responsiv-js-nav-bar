05da9693d3d26b03e673c0c9c0457390525f85af
Shafin_Mahmud <77242757+shafint@users.noreply.github.com>
2021-03-31 13:59:37 +0600

    Add files via upload

---
+++ responsive-nav/app.js
@@ -0,0 +1,15 @@
+const barger=document.querySelector(".hamberger");
+const menu=document.querySelector(".menu");
+
+
+
+
+
+
+barger.addEventListener("click",()=>{
+    menu.classList.toggle("trun")
+    barger.classList.toggle("roate")
+})
+
+
+   
